37a6414bc17bfa0e2510a585ec30fb8203070289
yassinfrh <yassin.frh@gmail.com>
2022-05-11 17:58:18 +0200

    created NewMatchActivity
    created NewMatchActivity but still have to implement it (finish intent in mainactivity)

--- app/src/main/java/com/example/easycalcio/activities/MainActivity.kt
+++ app/src/main/java/com/example/easycalcio/activities/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.easycalcio.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.easycalcio.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val floatingActionButton : FloatingActionButton = findViewById(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                //TODO: create intent to NewMatchActivity
+                val intent = Intent(this, NewMatchActivity::class.java)
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/easycalcio/activities/NewMatchActivity.kt
@@ -0,0 +1,13 @@
+package com.example.easycalcio.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.easycalcio.R
+
+class NewMatchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_match)
+        //TODO: implement new match
+    }
+}
